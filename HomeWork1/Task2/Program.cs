20e4a8cfe711d7ecb08ff94e55e0c0907e2bc8ce
Marat <108996479+Marat1988@users.noreply.github.com>
2023-01-27 15:46:55 +0200

    UPDATE

---
+++ HomeWork1/Task2/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+     /*Разработайте приложение, которое использует унаследованный код. Вам необходимо использовать функции
+     * FindWindow (поиск окна в системе), SendMessage (отсылка сообщений) из Windows API. Приложение должно произвести поиск окна вашего оконного приложения (можно
+     * реализовать его с помощью Windows Forms и т.д.). Если окно найдено необходимо послать ему сообщение в зависимости от выбора пользователя:
+     * ■ об изменении заголовка окна на заголовок, введенный пользователем о закрытии окна;
+     * ■ ваш вариант.*/
+    public class TestApi
+    {
+        [DllImport("User32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("User32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, string lParam);
+        [DllImport("User32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+    }
+    class Program
+    {
+        const string app = "Марат. Тестовое приложение";
+        delegate void Handler(IntPtr child);
+        static event Handler start;
+        static void Main(string[] args)
+        {
+            try
+            {
+                start += TestApp;
+                Console.Title = "Тест api-функций FindWindow и SendMessage";
+                Process p = Process.Start("TestApplication.exe");
+                Console.WriteLine("Для продолжения нажмите любую клавишу......");
+                Console.ReadKey();
+                IntPtr child = TestApi.FindWindow(null, app);
+                if (child == IntPtr.Zero)
+                {
+                    Console.WriteLine("Тестовое приложение не найдено. Продолжение не возможно!");
+                }
+                else
+                {
+                    start?.Invoke(child);
+                }
+            }
+            catch (Win32Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+        public static void TestApp(IntPtr child)
+        {
+            int WM_SETTEXT = 0x000C;
+            int WM_SYSCOMMAND = 0x0112;
+            int SC_CLOSE = 0xF060;
+            Console.WriteLine("Введите:\n1 - для закрытия окна дочернего приложения;\n2 - для изменения заголовка окна дочернего приложения;\n3 - для выхода.");
+            TestApi.SendMessage(child, 0x0102, 65, 0);
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int chooseUser))
+                {
+                    if (chooseUser == 3)
+                        break;
+                    switch (chooseUser)
+                    {
+                        case 1: TestApi.SendMessage(child, WM_SYSCOMMAND, SC_CLOSE, null);
+                            break;
+                        case 2:
+                            Console.Write("Введите сообщение: ");
+                            string msg = Console.ReadLine();                          
+                            TestApi.SendMessage(child, WM_SETTEXT, 0, msg);
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Не корректно набранная команда");
+                }
+            }
+        }
+    }
+}
---
+++ HomeWork1/TestApplication/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace TestApplication
+{
+    partial class FormTest
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonHello = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonHello
+            // 
+            this.buttonHello.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonHello.Location = new System.Drawing.Point(214, 114);
+            this.buttonHello.Name = "buttonHello";
+            this.buttonHello.Size = new System.Drawing.Size(185, 91);
+            this.buttonHello.TabIndex = 0;
+            this.buttonHello.Text = "Hello world!";
+            this.buttonHello.UseVisualStyleBackColor = true;
+            this.buttonHello.Click += new System.EventHandler(this.buttonHello_Click);
+            // 
+            // FormTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 376);
+            this.Controls.Add(this.buttonHello);
+            this.Name = "FormTest";
+            this.Text = "Марат. Тестовое приложение";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonHello;
+    }
+}
+

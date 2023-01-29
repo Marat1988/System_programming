0b4268a100e108bcb636d6e1cfb71f32f95e0151
Marat <pirat03071988@mail.ru>
2023-01-29 20:17:39 +0200

    UPDATE

--- HomeWork3-4/Task4/childApplication/Program.cs
+++ HomeWork3-4/Task4/childApplication/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace childApplication
@@ -13,11 +14,12 @@ namespace childApplication
         {
             if (args.Length > 0)
             {
+                Regex regex = new Regex(args[1], RegexOptions.IgnoreCase);
                 string textFile = File.ReadAllText(args[0]);
                 string[] mas = textFile.Split(' ');
-                int count = mas.Where(word => word == args[1]).Count();
+                int count = mas.Where(word => regex.IsMatch("Привет")).Count();
                 Console.WriteLine($"Заданное слово {args[1]} встречается в файле {args[0]} {count} раз");
-            }
+            }           
             Console.WriteLine("Для выхода жми любую клавишу......");
             Console.ReadKey();
         }

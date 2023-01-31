500bb38be753a3a0c9a423cb3cdf9ef2efd2febd
Marat <pirat03071988@mail.ru>
2023-01-31 23:58:00 +0200

    UPDATE

---
+++ HomeWork5-6/FormMain.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace HomeWork5_6
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxThread1 = new System.Windows.Forms.ListBox();
+            this.labelThread1 = new System.Windows.Forms.Label();
+            this.textBoxBeginRange = new System.Windows.Forms.TextBox();
+            this.textBoxEndRange = new System.Windows.Forms.TextBox();
+            this.labelBeginRange = new System.Windows.Forms.Label();
+            this.labelEndRange = new System.Windows.Forms.Label();
+            this.buttonRunThread = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxThread1
+            // 
+            this.listBoxThread1.FormattingEnabled = true;
+            this.listBoxThread1.Location = new System.Drawing.Point(15, 44);
+            this.listBoxThread1.Name = "listBoxThread1";
+            this.listBoxThread1.ScrollAlwaysVisible = true;
+            this.listBoxThread1.Size = new System.Drawing.Size(216, 355);
+            this.listBoxThread1.TabIndex = 0;
+            // 
+            // labelThread1
+            // 
+            this.labelThread1.AutoSize = true;
+            this.labelThread1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelThread1.Location = new System.Drawing.Point(12, 23);
+            this.labelThread1.Name = "labelThread1";
+            this.labelThread1.Size = new System.Drawing.Size(169, 15);
+            this.labelThread1.TabIndex = 1;
+            this.labelThread1.Text = "Поток 1. Простые числа";
+            // 
+            // textBoxBeginRange
+            // 
+            this.textBoxBeginRange.Location = new System.Drawing.Point(358, 41);
+            this.textBoxBeginRange.Name = "textBoxBeginRange";
+            this.textBoxBeginRange.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBeginRange.TabIndex = 2;
+            this.textBoxBeginRange.Text = "2";
+            this.textBoxBeginRange.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBoxBeginRange.UseWaitCursor = true;
+            this.textBoxBeginRange.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxBeginRange_KeyPress);
+            // 
+            // textBoxEndRange
+            // 
+            this.textBoxEndRange.Location = new System.Drawing.Point(358, 73);
+            this.textBoxEndRange.Name = "textBoxEndRange";
+            this.textBoxEndRange.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEndRange.TabIndex = 3;
+            this.textBoxEndRange.Text = "100000";
+            this.textBoxEndRange.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBoxEndRange.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxBeginRange_KeyPress);
+            // 
+            // labelBeginRange
+            // 
+            this.labelBeginRange.AutoSize = true;
+            this.labelBeginRange.Location = new System.Drawing.Point(258, 44);
+            this.labelBeginRange.Name = "labelBeginRange";
+            this.labelBeginRange.Size = new System.Drawing.Size(94, 13);
+            this.labelBeginRange.TabIndex = 4;
+            this.labelBeginRange.Text = "Нижняя граница:";
+            // 
+            // labelEndRange
+            // 
+            this.labelEndRange.AutoSize = true;
+            this.labelEndRange.Location = new System.Drawing.Point(258, 76);
+            this.labelEndRange.Name = "labelEndRange";
+            this.labelEndRange.Size = new System.Drawing.Size(96, 13);
+            this.labelEndRange.TabIndex = 5;
+            this.labelEndRange.Text = "Верхняя граница:";
+            // 
+            // buttonRunThread
+            // 
+            this.buttonRunThread.Location = new System.Drawing.Point(320, 99);
+            this.buttonRunThread.Name = "buttonRunThread";
+            this.buttonRunThread.Size = new System.Drawing.Size(98, 41);
+            this.buttonRunThread.TabIndex = 6;
+            this.buttonRunThread.Text = "Запуск";
+            this.buttonRunThread.UseVisualStyleBackColor = true;
+            this.buttonRunThread.Click += new System.EventHandler(this.buttonRunThread_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonRunThread);
+            this.Controls.Add(this.labelEndRange);
+            this.Controls.Add(this.labelBeginRange);
+            this.Controls.Add(this.textBoxEndRange);
+            this.Controls.Add(this.textBoxBeginRange);
+            this.Controls.Add(this.labelThread1);
+            this.Controls.Add(this.listBoxThread1);
+            this.Name = "FormMain";
+            this.Text = "Тестовое приложение. Многопоточность.";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxThread1;
+        private System.Windows.Forms.Label labelThread1;
+        private System.Windows.Forms.TextBox textBoxBeginRange;
+        private System.Windows.Forms.TextBox textBoxEndRange;
+        private System.Windows.Forms.Label labelBeginRange;
+        private System.Windows.Forms.Label labelEndRange;
+        private System.Windows.Forms.Button buttonRunThread;
+    }
+}
+
---
+++ HomeWork5-6/FormMain.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork5_6
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxBeginRange_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8)  //цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void buttonRunThread_Click(object sender, EventArgs e)
+        {
+            ThreadStart threadStart1 = new ThreadStart(thread1);
+            Thread threadNumber1 = new Thread(threadStart1);
+            threadNumber1.Start();
+        }
+
+        private void thread1()
+        {
+            int beginRange = int.Parse(textBoxBeginRange.Text);
+            int endRange = int.Parse(textBoxEndRange.Text);
+            for (int i = beginRange; i < endRange; i++)
+            {
+                if (checkPrimeNumber(i))
+                {
+                    listBoxThread1.Invoke(new Action(() =>
+                    {
+                        listBoxThread1.Items.Add(i);
+                    }));               
+                }
+            }
+        }
+
+        /// <summary>
+        /// Функция, которая проверяет, является ли число простым
+        /// </summary>
+        /// <param name="number">Вводимое число</param>
+        /// <returns>ИСТИНА, если число простое. В противном случае ЛОЖЬ</returns>
+        private bool checkPrimeNumber(int number)
+        {
+            if (number < 2)
+                throw new ArgumentException("Число должно быть больше либо равно 2");
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
